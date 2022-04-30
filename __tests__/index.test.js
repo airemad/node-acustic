f9307e15a40b7ae144c70cbb7ac3a2d8e290b77a
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-04-30 18:35:43 +0200

    feat: added core library tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,3 +1,18 @@
+const { getLastReport } = require('../src')
+const fixtures = require('../__fixtures__')
+
+beforeAll(() => {
+  jest.useFakeTimers('modern')
+  jest.setSystemTime(new Date(2021, 10, 3))
+})
+
+afterAll(() => {
+  jest.clearAllMocks()
+})
+
 describe('Acustic methods', () => {
-  it.todo('Should return a report')
+  it('Should return a report', async () => {
+    const report = await getLastReport()
+    expect(report).toStrictEqual(fixtures.outputReport)
+  })
 })
