4fcc35a23114f2af525eaa0fb8f42385614d5dff
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-04-30 17:31:05 +0200

    feat: added tests for validation

--- __tests__/utils.js/validation.test.js
+++ __tests__/utils.js/validation.test.js
@@ -1,4 +1,16 @@
+const { generateHash, checkReportFormat } = require('../../src/utils')
+const fixtures = require('../../__fixtures__')
+
 describe('Utils: Validation', () => {
-  it.todo('Should check the report')
-  it.todo('Should generate a valid hash')
+  it('Should check the report', () => {
+    expect(() => checkReportFormat(fixtures.outputReport.data)).not.toThrowError()
+  })
+
+  it('Should throw an error if the report is not valid', () => {
+    expect(() => checkReportFormat(undefined)).toThrowError('The Report is corrupted')
+  })
+
+  it('Should generate a valid hash', () => {
+    expect(generateHash(fixtures.sampleReport)).toBe(fixtures.sampleReportHash)
+  })
 })
