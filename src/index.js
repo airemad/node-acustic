62e71ac7a5c236dbd7864ba63b83dae7786063b1
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-04-30 18:36:03 +0200

    feat: added core library

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('Hello from Airemad/node-acustic!')
+const debug = require('debug')('airemad-acustic')
+
+const { downloadLastReport, parseReport, checkReportFormat } = require('./utils')
+
+const getLastReport = async () => {
+  debug('Requesting  the last report available has started')
+  const { content: rawReport, hash, source, timestamp } = await downloadLastReport()
+  const report = await parseReport(rawReport)
+  checkReportFormat(report)
+  debug('Requesting  the last report available has finished')
+  return {
+    data: report,
+    hash,
+    source,
+    timestamp
+  }
+}
+
+module.exports = {
+  getLastReport
+}
