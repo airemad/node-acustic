e077c68e453bc094aa9437788218f3f3d5bd0973
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2022-04-30 18:34:59 +0200

    feat: added tests for parser

--- __tests__/utils.js/parser.test.js
+++ __tests__/utils.js/parser.test.js
@@ -1,4 +1,17 @@
+const { parseReport, getPeriod } = require('../../src/utils')
+const fixtures = require('../../__fixtures__')
+
 describe('Utils: Parser', () => {
-  it.todo('Should parse the report')
-  it.todo('Should parse the period properly')
+  it('Should parse the report', async () => {
+    const report = await parseReport(fixtures.sampleReport)
+    expect(report).toStrictEqual(fixtures.outputReport.data)
+  })
+
+  it('Should parse the period properly', () => {
+    expect(getPeriod('INVENTED')).toBe(undefined)
+    expect(getPeriod('T')).toBe('total')
+    expect(getPeriod('D')).toBe('day')
+    expect(getPeriod('E')).toBe('evening')
+    expect(getPeriod('N')).toBe('night')
+  })
 })
